8f722dd940e314874fd8dc2c114750572116f1d9
chandchv <chandchv@gmail.com>
2021-12-31 17:10:06 +0530

    hotel
    laravel

---
+++ app/Http/Requests/RoomRequest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RoomRequest extends FormRequest
+{
+    public function authorize()
+    {
+        return true;
+    }
+
+    public function rules()
+    {
+        return [
+            'number'   => 'required|max:5|unique:rooms,number,'.(int) $this->id.',id,floor,'.$this->floor,
+            'floor'    => 'required|numeric|min:-999|max:999',
+			'type'    => 'required|string',
+            'capacity' => 'required|numeric|min:1|digits_between:1,2',
+            'price'    => 'required|regex:[^\d+[.,]?\d*$]|max:15',
+            'comment'  => 'nullable|string',
+        ];
+    }
+}
